e5a0b7024cce3bf4f940e1dc2159b4f8bda7650d
Gareth Mensah <gmensah@gmail.com>
2018-12-12 16:29:32 -0500

    QUick Fix.

--- daico/src/index.js
+++ daico/src/index.js
@@ -19,11 +19,12 @@ const avatarAddress = "0xcB4e66eCA663FDB61818d52A152601cA6aFEf74F";
 const votingMachineAddress = "0x0251bac7403718d49ccae5f8c02e03cd020f7f01";
 var daicoDAO;
 var icoScheme;
+var accounts = [];
 
 /*
 Helper function for initializing ArcJS and your app.
 */
-console.log("AA1");
+console.log("A1");
 async function initialize() {
   await InitializeArcJs({
     watchForAccountChanges: true,
@@ -36,19 +37,25 @@ async function initialize() {
     }
   });
 
-  console.log("AA2");
+  console.log("A2");
 
   // These are some basic configurations, feel free to edit as you need.
   // Learn more about the Arc.js configurations here: https://daostack.github.io/arc.js/Configuration/
 
-  await web3.eth.getAccounts(function(err, res) { accounts = res; });
+  await web3.eth.getAccounts(function(err, res) { 
+      console.log("A21");
+    accounts = res; 
+    console.log("A22");
+  });
 
-  console.log("AA3");
+  console.log("A3");
 
   ConfigService.set("estimateGas", true);
   ConfigService.set("txDepthRequiredForConfirmation", {kovan: 0, live: 0});
   AccountService.subscribeToAccountChanges(() => { window.location.reload(); });
   //LoggingService.logLevel = LogLevel.all;
+
+    console.log("A4");
 
   daicoDAO = await DAO.at(avatarAddress);
   const daoSchemes = await daicoDAO.getSchemes();
@@ -59,14 +66,16 @@ async function initialize() {
   ICOScheme.setProvider(web3.currentProvider);
   icoScheme = await ICOScheme.at(daoSchemeAddress);
 
-    // Gets the user reputation and the total reputation supply
-  var userAccount = "0xb0c908140fe6fd6fbd4990a5c2e35ca6dc12bfb2"; //web3.eth.accounts[0];
-  userRep = await getUserReputation(userAccount);
-  totalRep = web3.fromWei(await daicoDAO.reputation.getTotalSupply());
+  console.log("A5");
 
-  $("#userRep").text(
-    "Your Reputation: " + userRep + " rep (" + (userRep / totalRep) * 100 + "%)"
-  );
+    // Gets the user reputation and the total reputation supply
+  //var userAccount = "0xb0c908140fe6fd6fbd4990a5c2e35ca6dc12bfb2"; //web3.eth.accounts[0];
+  //userRep = await getUserReputation(userAccount);
+  //totalRep = web3.fromWei(await daicoDAO.reputation.getTotalSupply());
+
+  // $("#userRep").text(
+  //   "Your Reputation: " + userRep + " rep (" + (userRep / totalRep) * 100 + "%)"
+  // );
 }
 
 $(window).on("load", function() {
