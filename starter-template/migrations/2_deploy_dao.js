9a2fd3f20e0b409a4e625582e1dfe3542743ef0a
shivani <gupta.shivani254@gmail.com>
2019-06-17 19:42:11 +0530

    fix migration params error

--- starter-template/migrations/2_deploy_dao.js
+++ starter-template/migrations/2_deploy_dao.js
@@ -5,21 +5,17 @@ module.exports = async function(deployer) {
   DAOstackMigration.Ganache.server();
   DAOstackMigration.Ganache.provider();
   const options = {
-
     provider: 'http://localhost:8545',
     quiet: false,
     force: true,
     output: 'migration.json',
     privateKey: '0x4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d',
-    params: {
-      default: {
-        migrationSpec
-      }
-    }
+    params: migrationSpec
   };
 
   console.log("Here")
-  
+  const migrationBaseResult = await DAOstackMigration.migrateBase(options);
+  console.log("Here3")
   const migrationDAOResult = await DAOstackMigration.migrateDAO(options);
   console.log("Here2")
   const migrationDemoResult = await DAOstackMigration.migrateDemoTest(options);
