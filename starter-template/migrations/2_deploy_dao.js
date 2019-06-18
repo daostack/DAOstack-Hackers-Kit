0300cd28698a6e792ff394802fe5647991d98618
shivani <gupta.shivani254@gmail.com>
2019-06-18 15:13:43 +0530

    fix dev migration errors

--- starter-template/migrations/2_deploy_dao.js
+++ starter-template/migrations/2_deploy_dao.js
@@ -6,20 +6,19 @@ module.exports = async function(deployer) {
   DAOstackMigration.Ganache.provider();
   const options = {
     provider: 'http://localhost:8545',
+    gasPrice: 1,
     quiet: false,
     force: true,
     output: 'migration.json',
     privateKey: '0x4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d',
-    params: migrationSpec
+    prevmigration: 'migration.json',
+    params: {private: migrationSpec}
   };
 
-  console.log("Here")
   const migrationBaseResult = await DAOstackMigration.migrateBase(options);
-  console.log("Here3")
   const migrationDAOResult = await DAOstackMigration.migrateDAO(options);
   console.log("Here2")
   const migrationDemoResult = await DAOstackMigration.migrateDemoTest(options);
-  migrationDemoResult.test.Avatar // Test DAO avatar address
-  const migrationResult = await DAOstackMigration.migrate(options); // migrate
-  DAOstackMigration.cli()
+  //migrationDemoResult.test.Avatar // Test DAO avatar address
+  //const migrationResult = await DAOstackMigration.migrate(options); // migrate
 }
