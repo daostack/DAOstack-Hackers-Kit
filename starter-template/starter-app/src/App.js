a624a6494068d756e91928a882ec222269a12f3c
shivani <gupta.shivani254@gmail.com>
2019-06-20 19:05:25 +0530

    starter-template: initialize arc client and dockerize react app

--- starter-template/starter-app/src/App.js
+++ starter-template/starter-app/src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { Arc } from "@daostack/client";
+import { first } from 'rxjs/operators';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const settings = {
+  dev: {
+    graphqlHttpProvider: "http://127.0.0.1:8000/subgraphs/name/daostack",
+    graphqlWsProvider: "ws://127.0.0.1:8001/subgraphs/name/daostack",
+    web3Provider: "ws://127.0.0.1:8545",
+    ipfsProvider: "localhost",
+  }};
+
+async function initializeArc() {
+  const arc = new Arc(settings.dev);
+  await arc.initialize();
+  return arc;
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      arcIsInitialized: false,
+      arc: null,
+      daos: []
+    }
+  }
+
+  async componentWillMount() {
+    const arc = await initializeArc()
+    const daos = await arc.daos().pipe(first()).toPromise()
+    this.setState({
+      arcIsInitialized: true,
+      arc: arc,
+      daos: daos
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          {this.state.daos.map((dao) => (<li key={dao.address}> {dao.address} </li>))}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
