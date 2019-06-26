e753fcb4fbc63731f53b6c403c89d81b9bb5c6b6
shivani <gupta.shivani254@gmail.com>
2019-06-26 14:08:18 +0530

    starter-template: update testnet migration to use env variables

--- starter-template/truffle.js
+++ starter-template/truffle.js
@@ -8,13 +8,13 @@ module.exports = {
       network_id: "*"
     },
     // Use to deploy to the Kovan testnet using Infura
-    "kovan-infura": {
-      provider: () =>
-        new HDWalletProvider(
-          "<YOU-SEED-PHRASE>", // Seed phrase with ETH (to pay gas) in its first account
-          // Note that truffle uses the m/44'/60'/0'/0 Path to get the account to deploy with
-          "https://kovan.infura.io/<YOUR-INFURA-KEY>"
-        ),
+    // Note that truffle uses the m/44'/60'/0'/0 Path to get the account to deploy with
+    kovan: {
+
+      provider: () => new HDWalletProvider(
+        process.env.SEED_PHRASE,
+        process.env.KOVAN_INFURA_URL
+      ),
       network_id: 42,
       gasPrice: 1000000000 // 1 Gwei
     },
@@ -22,7 +22,7 @@ module.exports = {
 
       provider: () => new HDWalletProvider(
         process.env.SEED_PHRASE,
-        "https://rinkeby.infura.io/v3/e0cdf3bfda9b468fa908aa6ab03d5ba2"
+        process.env.RINKEBY_INFURA_URL
       ),
       network_id: 4,
       gas: 4612388 // Gas limit used for deploys
