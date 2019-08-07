a20093034e5bf1c472192115caa5e89199e3baf0
shivani <gupta.shivani254@gmail.com>
2019-08-07 18:08:40 +0530

    firestarter-example: use @daostack/migrations to deploy

--- firestarter-example/migrations/2_deploy_contracts.js
+++
@@ -1,5 +0,0 @@
-var Firestarter = artifacts.require("./Firestarter.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(Firestarter);
-};
---
+++ firestarter-example/migrations/DeployDAO.js
@@ -0,0 +1,34 @@
+const DAOstackMigration = require('@daostack/migration');
+const migrationSpec =  require('../data/fireStarterDAOspec.json')
+require('dotenv').config();
+
+async function migrate() {
+  const DEFAULT_GAS = 3.0
+
+  const options = {
+    provider: process.env.PROVIDER,
+    gasPrice: DEFAULT_GAS,
+    quiet: false,
+    force: true,
+    output: 'data/migration.json',
+    privateKey: process.env.PRIVATE_KEY,
+    customabislocation: 'build/contracts',
+    params: {
+      private: migrationSpec,
+      rinkeby: migrationSpec
+    },
+  };
+
+  switch (process.env.NETWORK) {
+    case "ganache":
+    case "private":
+      options.prevmigration = " ";
+      const migrationBaseResult = await DAOstackMigration.migrateBase(options);
+      console.log("deployed base")
+      options.prevmigration = options.output;
+      break;
+  }
+  const migrationDAOResult = await DAOstackMigration.migrateDAO(options);
+}
+
+migrate()
