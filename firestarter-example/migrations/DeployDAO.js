df46dee40159dab65ba88beaeca4be58c463bdcf
shivani <gupta.shivani254@gmail.com>
2020-01-20 15:49:52 +0530

    firestarter: update migration script

--- firestarter-example/migrations/DeployDAO.js
+++ firestarter-example/migrations/DeployDAO.js
@@ -3,30 +3,23 @@ const migrationSpec =  require('../data/fireStarterDAOspec.json')
 require('dotenv').config();
 
 async function migrate() {
-  const DEFAULT_GAS = 3.0
+  const DEFAULT_GAS = 3.5
 
   const options = {
     provider: process.env.PROVIDER,
     gasPrice: DEFAULT_GAS,
     quiet: false,
     force: true,
+    restart: true,
     output: process.env.OUTPUT_FILE,
     privateKey: process.env.PRIVATE_KEY,
-    customabislocation: process.env.CUSTOM_ABI_LOCATION,
+    customAbisLocation: process.env.CUSTOM_ABI_LOCATION,
     params: {
       private: migrationSpec,
       rinkeby: migrationSpec
     },
   };
 
-  switch (process.env.NETWORK) {
-    case "ganache":
-    case "private":
-      options.prevmigration = " ";
-      const migrationBaseResult = await DAOstackMigration.migrateBase(options);
-      options.prevmigration = options.output;
-      break;
-  }
   const migrationDAOResult = await DAOstackMigration.migrateDAO(options);
 }
 
