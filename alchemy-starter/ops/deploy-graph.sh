2aa8163a8da88c17a2974c51f6b9c1744e8a9652
shivani <gupta.shivani254@gmail.com>
2020-01-23 15:52:05 +0530

    remove unecessary example

--- alchemy-starter/ops/deploy-graph.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-current_directory=`pwd`
-
-cd subgraph
-npm i
-opts='{ "migrationFile" : "'$current_directory'/data/migration.json", "subgraphName" : "daostack" }'
-npm run deploy "$opts"
